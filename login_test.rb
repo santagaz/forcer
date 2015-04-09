cb75f7e41c1c54d0c3a69e18a90e7a9476dfa4d7
feline <gaziztazhenov@gmail.com>
2015-04-09 02:04:54 -0700

    attempt to list metadata

--- login_test.rb
+++ login_test.rb
@@ -1,16 +1,44 @@
 require 'savon'
 
-partner_headers = {
+API_VERSION = 33.0
+
+enterprise_headers = {
   "Authentication" => "secret"
 }
-partner = Savon.client(wsdl: "/Users/gt/Downloads/partner.wsdl", headers: partner_headers)
-# p partner.operations
+enterprise_client = Savon.client(wsdl: "/Users/gt/Downloads/enterprise.wsdl.xml", headers: enterprise_headers)
+# p enterprise.operations
 # username: "gaziz@eventbrite.com.comitydev", password: "?kMMTR[d}X7`Fd}>@T.fpX1t6k2We39Qtq42NKbnLWSQ"
+
 message = {
   username: "gaziz@eventbrite.com.comitydev",
   password: "?kMMTR[d}X7`Fd}>@T.fpX1t6k2We39Qtq42NKbnLWSQ"
 }
 
-response = partner.call(:login, message: message)
-session_id = response.body[:login_response][:result][:session_id]
-p "session id = #{session_id}"
+# === login
+response = enterprise_client.call(:login, message: message)
+current_session_id = response.body[:login_response][:result][:session_id]
+server_url = response.body[:login_response][:result][:server_url]
+server_url = server_url.split("\/services")[0]
+#current_session_id = current_session_id[0, 15]
+# p "session id = #{current_session_id}"
+# p "body = #{response.body}"
+#p "server url = #{server_url}"
+
+options = {
+  wsdl: "/Users/gt/Downloads/metadata.wsdl",
+  endpoint: "#{server_url}/services/Soap/c/#{API_VERSION}",
+  soap_header: {
+    "SessionHeader" => {
+      "sessionId" => current_session_id
+    }
+  }
+}
+client = Savon.client(options)
+
+
+# === list metadata
+queries = "<met:type>CustomObject</met:type><met:folder>CustomObject</met:folder>"
+list_metadata_request = File.read("./list_metadata_request.xml");
+xml_param = list_metadata_request % [current_session_id, queries]
+response = client.call(:list_metadata, :xml => xml_param)
+p response
