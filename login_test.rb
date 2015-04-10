0a6cbdad62fd177d11606e754302016aa8a1360d
feline <gaziztazhenov@gmail.com>
2015-04-09 19:46:55 -0700

    fixed error in code. used wrong endpoints.

--- login_test.rb
+++ login_test.rb
@@ -2,10 +2,15 @@ require 'savon'
 
 API_VERSION = 33.0
 
-enterprise_headers = {
-  "Authentication" => "secret"
+endpoint_url = "https://test.salesforce.com"
+options = {
+  endpoint: "#{endpoint_url}/services/Soap/c/#{API_VERSION}",
+  wsdl: "./enterprise.wsdl", 
+  :headers => {
+    "Authentication" => "secret"
+  }
 }
-enterprise_client = Savon.client(wsdl: "/Users/gt/Downloads/enterprise.wsdl.xml", headers: enterprise_headers)
+enterprise_client = Savon.client(options)
 # p enterprise.operations
 # username: "gaziz@eventbrite.com.comitydev", password: "?kMMTR[d}X7`Fd}>@T.fpX1t6k2We39Qtq42NKbnLWSQ"
 
@@ -17,21 +22,21 @@ message = {
 # === login
 response = enterprise_client.call(:login, message: message)
 current_session_id = response.body[:login_response][:result][:session_id]
-server_url = response.body[:login_response][:result][:server_url]
-server_url = server_url.split("\/services")[0]
-#current_session_id = current_session_id[0, 15]
+metadata_server_url = response.body[:login_response][:result][:metadata_server_url]
 # p "session id = #{current_session_id}"
 # p "body = #{response.body}"
 #p "server url = #{server_url}"
 
 options = {
-  wsdl: "/Users/gt/Downloads/metadata.wsdl",
-  endpoint: "#{server_url}/services/Soap/c/#{API_VERSION}",
+  wsdl: "./metadata.wsdl",
+  endpoint: metadata_server_url,
   soap_header: {
-    "SessionHeader" => {
-      "sessionId" => current_session_id
+    "tns:SessionHeader" => {
+      "tns:sessionId" => current_session_id
     }
-  }
+  },
+  read_timeout: 60 * 10,
+  open_timeout: 60 * 10
 }
 client = Savon.client(options)
 
@@ -40,5 +45,6 @@ client = Savon.client(options)
 queries = "<met:type>CustomObject</met:type><met:folder>CustomObject</met:folder>"
 list_metadata_request = File.read("./list_metadata_request.xml");
 xml_param = list_metadata_request % [current_session_id, queries]
+#p "xml request body: #{xml_param}"
 response = client.call(:list_metadata, :xml => xml_param)
 p response
