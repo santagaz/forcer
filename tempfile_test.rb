df691c01626908c87afa9bf7355651f59384e77b
feline <gaziztazhenov@gmail.com>
2015-04-12 19:31:14 -0700

    added module to temp_filename and method to create full path with name of file

--- tempfile_test.rb
+++ tempfile_test.rb
@@ -1,11 +1,21 @@
 require 'tmp_file'
 
-path = "/tmp"
-def random_filename(extension = 'zip')
-  "#{SecureRandom.urlsafe_base64}.#{extension}"
+module FileNameUtility
+  PATH = "/tmp"
+  def random_filename(extension = 'zip')
+    "#{SecureRandom.urlsafe_base64}.#{extension}"
+  end
+
+  def tempfile_name(extension = 'zip')
+    "#{PATH}/#{random_filename(extension)}"
+  end
+
 end
 
-filename = "#{path}/#{random_filename}"
+#filename = "#{path}/#{random_filename}"
+=begin
+include FileNameUtility
+filename = tempfile_name
 File.open(filename, 'wb') do |file|
   file.write("random data")
 
@@ -13,3 +23,4 @@ File.open(filename, 'wb') do |file|
   p "file content : #{file}"
 end
 FileUtils.rm_f filename
+=end
