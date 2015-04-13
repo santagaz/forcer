854df0331d99e5b31f928d3ef0d5a2ca5fbcbd86
feline <gaziztazhenov@gmail.com>
2015-04-12 19:31:41 -0700

    successfull first zip creation

--- zip_project_test.rb
+++ zip_project_test.rb
@@ -1,3 +1,15 @@
-require "tempfile_test.rb"
+require "./tempfile_test.rb"
+require "zip"
 
+include FileNameUtility
 
+folder = "./" # current folder
+input_filename = "test_text_file.txt"
+zipfile_name = tempfile_name
+
+Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
+  zipfile.add(input_filename, folder + input_filename)
+  p zipfile
+end
+
+FileUtils.rm zipfile_name
