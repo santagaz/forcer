fdf131bfeb9a36b68aab37add1604e98b6f8719c
gazazello <gaziztazhenov@gmail.com>
2015-04-14 16:53:18 -0700

    little improvement to metadata_service spec, added proper login_call xml fixture

--- spec/lib/metadata_services/metadata_service_spec.rb
+++ spec/lib/metadata_services/metadata_service_spec.rb
@@ -1,8 +1,28 @@
-require 'rspec'
-require File.expand_path("../../../../lib/metadata_services/metadata_service", __FILE__)
+require "spec_helper"
+require_relative "../../../lib/metadata_services/metadata_service"
+require "savon/mock/spec_helper"
 
 describe Metadata::MetadataService do
-  it "authenticates in sfdc org" do
-    client = Metadata::MetadataService.new(File.expand_path("../../../fixtures/TestProject", __FILE__))
+
+  include Savon::SpecHelper
+
+  before(:all) do
+    savon.mock!
+    fixture_login_response = File.read("spec/fixtures/login_response.xml")
+    savon.expects(:login).with(anything).returns(fixture_login_response)
   end
+
+  after(:all) { savon.unmock! }
+
+  # runs after savon is mocked
+  let(:service) { Metadata::MetadataService.new(
+      File.expand_path("../../../fixtures/TestProject", __FILE__)
+  ) }
+
+  describe "#initialize" do
+    it "authenticates in sfdc org" do
+      expect(service.current_session_id).to be("test_session_id")
+    end
+  end
+
 end
