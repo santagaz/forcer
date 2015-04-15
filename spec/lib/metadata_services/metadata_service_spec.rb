4fec884455eb0111506a6bc17361b77a55c95b66
feline <gaziztazhenov@gmail.com>
2015-04-15 02:28:45 -0700

    fixed double request for test case

--- spec/lib/metadata_services/metadata_service_spec.rb
+++ spec/lib/metadata_services/metadata_service_spec.rb
@@ -6,41 +6,47 @@ describe Metadata::MetadataService do
 
   include Savon::SpecHelper
 
-  let(:args) {
-    {
-      host: "https://fake.salesforce.com",
-      username: "test_username",
-      password: "test_password",
-      security_token: "test_token"
-    }
-  }
+  # let(:args) {
+  #   {
+  #     host: "https://fake.salesforce.com",
+  #     username: "test_username",
+  #     password: "test_password",
+  #     security_token: "test_token"
+  #   }
+  # }
 
   # prepare savon mock for soap calls
   before(:all) do
     savon.mock!
+    args = {
+        host: "https://fake.salesforce.com",
+        username: "test_username",
+        password: "test_password",
+        security_token: "test_token"
+    }
     fixture_login_response = File.read("spec/fixtures/login_response.xml")
     message = {
         username: "test_username",
         password: "test_passwordtest_token"
     }
     savon.expects(:login).with(message: message).returns(fixture_login_response)
+    @service = Metadata::MetadataService.new(
+      File.expand_path("../../../fixtures/TestProject", __FILE__),
+      args)
   end
 
   after(:all) { savon.unmock! }
 
   # runs after savon is mocked
-  let(:service) { Metadata::MetadataService.new(
-      File.expand_path("../../../fixtures/TestProject", __FILE__),
-      args
-  ) }
+  # let(:service) {  }
 
   describe "#initialize" do
-    it "authenticates in sfdc org" do
-      expect(service.current_session_id).to eq("test_session_id")
+    it "authenticates" do
+      expect(@service.current_session_id).to eq("test_session_id")
     end
 
-    it "creates soap metadata client" do
-      # expect(service.metadata_client).to be(not(nil))
+    it "creates metadata client" do
+      expect(@service.metadata_client.operations).to
     end
   end
 
