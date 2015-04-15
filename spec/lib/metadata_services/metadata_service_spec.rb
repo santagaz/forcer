7ac6190607af163db92faec759a6f88c2ec8838b
gazazello <gaziztazhenov@gmail.com>
2015-04-15 10:59:59 -0700

    Fixed test mock exception replacing savon mock with standard rspec double. Created fixture project testing deployment.

--- spec/lib/metadata_services/metadata_service_spec.rb
+++ spec/lib/metadata_services/metadata_service_spec.rb
@@ -19,26 +19,25 @@ describe Metadata::MetadataService do
   before(:all) do
     savon.mock!
     args = {
-        host: "https://fake.salesforce.com",
-        username: "test_username",
-        password: "test_password",
-        security_token: "test_token"
+      host: "https://fake.salesforce.com",
+      username: "test_username",
+      password: "test_password",
+      security_token: "test_token"
     }
     fixture_login_response = File.read("spec/fixtures/login_response.xml")
-    message = {
-        username: "test_username",
-        password: "test_passwordtest_token"
+
+    login_info = {
+      username: args[:username],
+      password: "#{args[:password]}#{args[:security_token]}"
     }
-    savon.expects(:login).with(message: message).returns(fixture_login_response)
+    savon.expects(:login).with(message: login_info).returns(fixture_login_response)
+
     @service = Metadata::MetadataService.new(
       File.expand_path("../../../fixtures/TestProject", __FILE__),
       args)
   end
 
   after(:all) { savon.unmock! }
-
-  # runs after savon is mocked
-  # let(:service) {  }
 
   describe "#initialize" do
     it "authenticates" do
@@ -50,4 +49,14 @@ describe Metadata::MetadataService do
     end
   end
 
+  describe "#deploy" do
+    before(:each) do
+      @metadata = double()
+      @service.metadata_client = @metadata
+    end
+
+    it "queues deployment" do
+      allow(@metadata).to receive(:deploy).with(any_args) { "Queued" }
+    end
+  end
 end
