569ee9e0eb37a356af08d7973085d96f95ff0119
gazazello <gaziztazhenov@gmail.com>
2015-04-16 13:57:37 -0700

    replace custom mocks with rspec mocks

--- spec/lib/metadata_services/metadata_service_spec.rb
+++ spec/lib/metadata_services/metadata_service_spec.rb
@@ -47,13 +47,46 @@ describe Metadata::MetadataService do
     before(:each) do
       @metadata = double()
       @service.metadata_client = @metadata
+
+      @mock_deploy_response = double()
+      allow(@mock_deploy_response).to receive(:body) do
+        {
+          :deploy_response => {
+            :result => {
+              :state=> "Queued",
+              :done=> "false"
+            }
+          }
+        }
+      end
+
+      @mock_list_response = double()
+      allow(@mock_list_response).to receive(:body) do
+        {
+          :list_metadata_response=>{
+            :result=> [{
+               :created_by_id=>"test_user_id",
+               :created_by_name=>"test_user_firstname test_user_lastname",
+               :created_date=>"#<DateTime: 2015-04-15T05:15:21+00:00>",
+               :file_name=>"objects/TestSObject__c.object",
+               :full_name=>"TestSObject__c",
+               :id=>"test_sobject_id",
+               :last_modified_by_id=>"test_user_id",
+               :last_modified_by_name=>"test_user_firstname test_user_lastname",
+               :last_modified_date=>"#<DateTime: 2015-04-15T05:30:01+00:00>",
+               :manageable_state=>"unmanaged",
+               :type=>"CustomObject"
+             }]
+          }
+        }
+      end
     end
 
     describe "#deploy" do
       it "receives xml template" do
         allow(@metadata).to receive(:call).with(:deploy, any_args) do |name, message|
           expect(message[:xml]).to include_xml_tag("<met:sessionId>test_session_id</met:sessionId>")
-          Forcer::MockResponse.new(:deploy)
+          @mock_deploy_response
         end
 
         @service.deploy
@@ -61,7 +94,7 @@ describe Metadata::MetadataService do
 
       it "queues deployment" do
         allow(@metadata).to receive(:call).with(:deploy, any_args) do
-          Forcer::MockResponse.new(:deploy)
+          @mock_deploy_response
         end
         expect(@service.deploy.body[:deploy_response][:result][:state]).to eq("Queued")
       end
@@ -71,14 +104,14 @@ describe Metadata::MetadataService do
       it "prepares xml with types" do
         allow(@metadata).to receive(:call).with(:list_metadata, any_args) do |name, message|
           expect(message[:xml]).to include_xml_tag("<met:type>CustomObject</met:type>")
-          Forcer::MockResponse.new(:list_metadata)
+          @mock_list_response
         end
         @service.list
       end
 
       it "lists metadata objects" do
         # @args specified request only for CustomObject
-        allow(@metadata).to receive(:call).with(:list_metadata, any_args) { Forcer::MockResponse.new(:list_metadata) }
+        allow(@metadata).to receive(:call).with(:list_metadata, any_args) { @mock_list_response }
         expect(@service.list.body[:list_metadata_response][:result].first[:full_name].to_s).to eq("TestSObject__c")
       end
     end
