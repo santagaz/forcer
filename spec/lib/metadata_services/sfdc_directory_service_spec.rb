6f02e3c2b2ea6a798e4fafdee7e46d7a32936f1c
gazazello <gaziztazhenov@gmail.com>
2015-04-16 16:54:11 -0700

    started test for directory/zip service of sfdc project

---
+++ spec/lib/metadata_services/sfdc_directory_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require_relative "../../../lib/metadata_services/sfdc_directory_service"
+
+describe 'Metadata::SfdcDirectoryService' do
+
+  it 'should do something' do
+
+    true.should == false
+  end
+end
