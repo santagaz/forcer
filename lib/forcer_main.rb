3444bab7f955555f4455395c2b1174068841b647
feline <gaziztazhenov@gmail.com>
2015-04-21 04:47:04 -0700

    first successfull deployment using configuration yaml and command line options

--- lib/forcer_main.rb
+++ lib/forcer_main.rb
@@ -1,26 +1,32 @@
-require "forcer/version"
 require "thor"
+require_relative "./forcer/version"
 require_relative "utilities/action_options_service"
+require_relative "./metadata_services/metadata_service"
 
 module Forcer
   class ForcerMain < Thor
-    class_option :dest, required: true
+    class_option :dest
     class_option :config
 
     option :source
     desc "deploy --dest destination_org_name", "Deploys project on local machine to destination org. Destination org" +
        " name should be specified in configuration.yml. Forcer asks for any information missing from configuration.yml"
     def deploy
-      p "before options => #{options}"
-      verify_options(options)
-      p "after options => #{options}"
+      all_options = verify_options(options)
+      metadata = Metadata::MetadataService.new(all_options[:source], all_options)
+      metadata.deploy
     end
 
 
     no_commands do
-      def verify_options(options = {})
-        ActionOptionsService.load_config_file(options)
-        #options[:dest_url] ||= ask("Enter destination org url (test.salesforce.org or login.salesforce.org): ")
+      def verify_options(old_options = {})
+        new_options = ActionOptionsService.load_config_file(old_options)
+        new_options[:host] ||=  "https://" + ask("Enter org url (test.salesforce.org or login.salesforce.org): ")
+        new_options[:username] ||= ask("Enter username: ")
+        new_options[:password] ||= ask("Enter password: ")
+        new_options[:security_token] ||= ask("Enter security token: ")
+        new_options[:source] ||= Dir.pwd
+        return new_options
       end
     end
   end
