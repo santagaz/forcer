6911d7bdeae89a91d23e9a2b293c6477dbcf1db2
feline <gaziztazhenov@gmail.com>
2015-04-21 04:17:48 -0700

    fixed key format error for command line options

--- lib/utilities/action_options_service.rb
+++ lib/utilities/action_options_service.rb
@@ -5,7 +5,10 @@ module Forcer
 
     # attempts to read salesforce org information from yaml
     def self.load_config_file(old_options = {})
-      options = old_options.clone
+      options = {}
+      old_options.each do |k, v|
+        options.store(k.to_sym, v)
+      end
 
       config_file_path = File.join(Dir.pwd, "/configuration.yml")
       return options unless File.exists?(config_file_path)
