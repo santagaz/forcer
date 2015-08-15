43e53f43fb97165c31049361dd250d4fe1d72fe0
GazizAtEventbrite <gaziz@eventbrite.com>
2015-08-14 19:13:26 -0700

    added unit test for production specific scenario, when all xml_snippets should be deployed without exceptions

--- spec/lib/metadata_services/sfdc_directory_service_spec.rb
+++ spec/lib/metadata_services/sfdc_directory_service_spec.rb
@@ -6,18 +6,7 @@ require 'nokogiri'
 describe 'Metadata::SfdcDirectoryService' do
 
   before(:all) do
-    @test_project_path = File.expand_path("../../../fixtures/TestProject", __FILE__)
-    @exclude_component_path = File.expand_path("../../../fixtures/exclude_components.yml", __FILE__)
-    @exclude_xml_path = File.expand_path("../../../fixtures/exclude_xml_nodes.yml", __FILE__)
-    args = {
-        exclude_components: @exclude_component_path,
-        exclude_xml: @exclude_xml_path,
-        source: @test_project_path
-    }
-    @directory_service = Metadata::SfdcDirectoryService.new(args)
-    @path_package_xml = File.join(@test_project_path, "/project/src/package.xml")
-    @temp_zip_filename = @directory_service.make_project_zip
-    @zip_file = Zip::File.open(@temp_zip_filename)
+    init_directory_service("test.salesforce.com")
   end
 
   after(:all) do
@@ -60,10 +49,21 @@ describe 'Metadata::SfdcDirectoryService' do
     end
   end
 
+  # by default tests run for sandbox
   describe "xml filter" do
-    it "extracts xml snippet from file" do
+    before(:is_production => true) do
+      init_directory_service("login.salesforce.com")
+    end
+
+    it "extracts xml snippet from file for sandbox" do
       doc = Nokogiri::XML(@zip_file.read('profiles/Admin.profile'))
       expect(doc.search("*//layoutAssignments/layout[starts-with('Social')]")).to be_empty
+    end
+
+    it "ignores exclude_xml for production", is_production: true do
+      doc = Nokogiri::XML(@zip_file.read('profiles/Admin.profile'))
+      expect(@args[:host]).to start_with("login")
+      expect(doc.search("*//layoutAssignments/layout[starts-with('Social')]")).to_not be_empty
     end
 
     it "saves original into file" do
@@ -72,6 +72,22 @@ describe 'Metadata::SfdcDirectoryService' do
   end
 
   private
+
+  def init_directory_service(deployment_host)
+    @test_project_path = File.expand_path("../../../fixtures/TestProject", __FILE__)
+    @exclude_component_path = File.expand_path("../../../fixtures/exclude_components.yml", __FILE__)
+    @exclude_xml_path = File.expand_path("../../../fixtures/exclude_xml_nodes.yml", __FILE__)
+    @args = {
+        exclude_components: @exclude_component_path,
+        exclude_xml: @exclude_xml_path,
+        source: @test_project_path,
+        host: deployment_host
+    }
+    @directory_service = Metadata::SfdcDirectoryService.new(@args)
+    @path_package_xml = File.join(@test_project_path, "/project/src/package.xml")
+    @temp_zip_filename = @directory_service.make_project_zip
+    @zip_file = Zip::File.open(@temp_zip_filename)
+  end
 
   def print_zip_content(zip_filename)
     Zip::File.foreach(zip_filename) do |entry|
